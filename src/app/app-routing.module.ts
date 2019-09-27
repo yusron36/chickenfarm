7e38428a104b388efc441ba6ac8873de1fe6c925
yusron nasrullah <yusron@gmail.com>
2019-09-27 16:49:58 +0700

    all perubahan

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UserComponent } from './user/user.component';
-import { PenjualanComponent } from './penjualan/penjualan.component';
-import { HomeComponent } from './home/home.component';
-import { BarangComponent } from './barang/barang.component';
-import { KategoriComponent } from './kategori/kategori.component';
-import { SatuanComponent } from './satuan/satuan.component';
-import { PenerimaanComponent } from './penerimaan/penerimaan.component';
 
 
-const routes: Routes = [
-  {path:'user',component:UserComponent,children:[
-    {path:'buyer',component:PenjualanComponent}
-  ]},
-  {path:'admin',component:HomeComponent,children:[
-    {path:'masterbarang',component:BarangComponent},
-    {path:'masterkategori',component:KategoriComponent},
-    {path:'satuan',component:SatuanComponent},
-    {path:'penerimaan',component:PenerimaanComponent}
-  ]}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UserComponent } from './user/user.component';
-import { BarangComponent } from './barang/barang.component';
-import { SatuanComponent } from './satuan/satuan.component';
-import { KategoriComponent } from './kategori/kategori.component';
-import { PenjualanComponent } from './penjualan/penjualan.component';
-import { PenerimaanComponent } from './penerimaan/penerimaan.component';
-import { LandingpageComponent } from './landingpage/landingpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    UserComponent,
-    BarangComponent,
-    SatuanComponent,
-    KategoriComponent,
-    PenjualanComponent,
-    PenerimaanComponent,
-    LandingpageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [LandingpageComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
